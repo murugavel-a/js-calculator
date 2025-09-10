05f09cfed258b154883692eec4a13e7f3b9dd8e5
Murugavel-A <128735644+murugavel-a@users.noreply.github.com>
2025-09-10 18:30:07 +0530

    Add files via upload

---
+++ js-calculator.js
@@ -0,0 +1,21 @@
+const display = document.querySelector("#display");
+
+function toDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "error";
+  }
+}
+
+function plusOrMinus() {
+  display.value = string(-Number(display.value));
+}
